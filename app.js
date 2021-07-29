10647769597120d52dc615f2bf1acd4fa461b8a4
Simone Taeggi <startae14@gmail.com>
2021-07-30 00:34:15 +0200

    MAIN 06: define the first route, with a controller

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 
+// retrieve feeds
+const feedRoutes = require('./routes/feed');
+
 const app = express();
+
+app.use('/feed', feedRoutes);
 
 app.listen(8080);
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,22 @@
 exports.getPost = (req, res, next) => {
   res.json({
+    posts: [
+      {
+        id: 1,
+        title: 'Titolo 1',
+        description: 'description'
+      },
+      {
+        id: 2,
+        title: 'Titolo 1',
+        description: 'description'
+      },
+      {
+        id: 3,
+        title: 'Titolo 1',
+        description: 'description'
+      }
+    ],
     test: 'test'
   });
 };
